6d7df5576c1b75dfac27fcb0540a29b8529ea783
Alae-Eddine444 <al.mrabti@edu.umi.ac.ma>
2026-03-27 12:01:34 +0100

    verification de Pipeline

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS flaskdb;
+USE flaskdb;
+
+CREATE TABLE IF NOT EXISTS USERS (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email TEXT
+);
+
+INSERT INTO users (name) VALUES ('Salim'), ('Nour'), ('Youssef');
